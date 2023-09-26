dbc87dbd2e1bb4eb33b8ceaab8209de310d57eb2
camilo mendoza <143355526+Carpediem-code234@users.noreply.github.com>
2023-09-26 17:32:43 -0500

    Add files via upload

---
+++ Almacen/src/Producto.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Camilo Mendoza
+ */
+public class Producto {
+
+    private String nombre;
+    private String tipo;
+    private int cantidad;
+    
+    
+    
+    public Producto(String nombre, String tipo, int cantidad) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.cantidad = cantidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+    
+    
+   
+}
